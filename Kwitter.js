1692d58e8901674a352f85e8e846fed3e4b7ecd0
ඞඞAdi Mominඞඞ <120671093+AdiMomin@users.noreply.github.com>
2023-05-30 07:34:25 -0400

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnLCKUm1LeUmlD_Mzh1t4vS5xkaOKSgdg",
+  authDomain: "lezchat-1a11b.firebaseapp.com",
+  projectId: "lezchat-1a11b",
+  storageBucket: "lezchat-1a11b.appspot.com",
+  messagingSenderId: "328668089429",
+  appId: "1:328668089429:web:85116ae066a00bd92318dc",
+  measurementId: "G-HPXBBVP0EY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
